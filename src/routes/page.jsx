ac2d1dfd1823611edee9a030112564ec5acfbc99
Bee <mrhumanman2@gmail.com>
2022-01-28 12:25:55 -0500

    Start adding content to page.jsx

--- src/routes/page.jsx
+++ src/routes/page.jsx
@@ -1,1 +1,15 @@
-export default function Page() {}
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Helemt } from "react-helmet";
+import { promises } from "fs";
+
+export default async function Page() {
+	const [slug, setSlug] = useState("");
+	const paramSlug = useParams().slug;
+
+	useEffect(() => {
+		setSlug(paramSlug);
+	}, [setSlug, paramSlug]);
+
+	const exists = !(await promises.access(`../pages/`));
+}
